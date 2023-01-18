75a26240a23516b10e8d6daf0d1c8a4590747808
David Pineiro <david@davidpineiro.xyz>
2023-01-18 00:24:44 -0500

    added exec stack instructions

---
+++ VirtualMachine/src/main/java/xyz/davidpineiro/jpush/vm/instruction/exec/ExecConfig.java
@@ -0,0 +1,11 @@
+package xyz.davidpineiro.jpush.vm.instruction.exec;
+
+import xyz.davidpineiro.jpush.vm.instruction.annotationHelper.GenerateStackIntructions;
+
+@GenerateStackIntructions(
+        stack_name = "Exec",
+        stack_java_type = "Instruction",
+        stack_vm_name = "execStack"
+)
+public class ExecConfig {
+}
